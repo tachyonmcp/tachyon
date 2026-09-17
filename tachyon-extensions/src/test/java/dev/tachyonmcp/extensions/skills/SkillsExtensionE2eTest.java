62cb71fa34a9688f2b57a61c63fd324a38482973
Konstantin Pavlov <1517853+kpavlov@users.noreply.github.com>
2026-09-17 23:04:26 +0300

    review findings

--- tachyon-extensions/src/test/java/dev/tachyonmcp/extensions/skills/SkillsExtensionE2eTest.java
+++ tachyon-extensions/src/test/java/dev/tachyonmcp/extensions/skills/SkillsExtensionE2eTest.java
@@ -56,7 +56,7 @@ class SkillsExtensionE2eTest {
             new CompositeSkillsRegistry(filesystemSkillsRegistry, classpathSkillsRegistry);
 
     // quickstart example
-    public void main(String... args) throws InterruptedException {
+    public static void main(String... args) throws InterruptedException {
         try (var server = TachyonServer.builder()
                 .port(8080)
                 .withExtensions(SkillsExtension.builder()
